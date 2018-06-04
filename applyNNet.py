0edaa666fbb108bdec6751b1b35c153f406f0923
Carlos A. Ledezma <caledezma@gmail.com>
2018-06-04 11:37:39 +0100

    Add files via upload

---
+++ applyNNet.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 14:43:52 2017
+
+Apply forward propagation on data, reading the calculated weights from a
+previously saved file
+
+@author: cledezma
+"""
+
+import nnetFunc
+import h5func
+from numpy import atleast_2d
+from numpy import hstack
+
+# Read weights
+fname = 'optimWeights/TPECGmildIschemia/featChange/optimalWeightsTPECGmildIschemia3hl9hu.h5'
+vname = ['W','IN','OUT','Precision','Recall']
+W, IN, OUT,p,r = h5func.import_h5(fname,vname)[0]
+
+# Read data
+fname = 'trainingData/pECGfeat/TPtrainingSetMildIschemiaFeatChange.h5'
+vname = ['data']
+dat = h5func.import_h5(fname,vname)[0][0]
+
+feat = dat[:,:-4]
+target = atleast_2d(dat[:,-4:]).T
+
+# Apply forward prop
+forwardProp = nnetFunc.fProp()
+o = forwardProp(feat,target,IN,W,OUT,0)[0].T
+
+# Compare results with targets
+comp = hstack((o,target.T))
